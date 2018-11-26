f9469641eb644e1445168e96cf8e6edff8c0cfbd
awohlford <34891927+awohlford@users.noreply.github.com>
2018-11-26 01:27:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ input = open("input_file.txt", "r")
 output = open("output_file.txt", "w")
 
 first_line = input.readline().split()
-graph.add_nodes_from([1, first_line[0]])
 
 second_line = input.readline().split()
-#for x in range(int(first_line[0])):
-    #graph[x + 1]['color'] = second_line[x]
+for x in range(int(first_line[0])):
+    graph.add_node(x + 1, second_line[x])
 
 for line in input.readlines():
     line = line.split()
