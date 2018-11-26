f3b21bbc7278d3c946e3ea6425a11007f3e851e3
awohlford <34891927+awohlford@users.noreply.github.com>
2018-11-26 01:06:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import networkx as nx
 
+graph = nx.Graph()
+
 input = open("input_file.txt", "r")
 output = open("output_file.txt", "w")
 
+first_line = input.readline().split()
+graph.add_nodes([0, first_line[0]])
+
 for line in input.readlines():
-    
+    line = line.split()
+    graph.add_edge(line[0], line[1])
 
 input.close()
 output.close()
+
+print(graph.number_of_nodes())
+print(graph.number_of_edges())
