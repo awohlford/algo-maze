fcae4d2f04f22ba8a9ef722ff930b389497f819e
awohlford <34891927+awohlford@users.noreply.github.com>
2018-11-26 01:16:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ output = open("output_file.txt", "w")
 first_line = input.readline().split()
 graph.add_nodes([0, first_line[0]])
 
+input.readline()
+
 for line in input.readlines():
     line = line.split()
     graph.add_edge(line[0], line[1])
