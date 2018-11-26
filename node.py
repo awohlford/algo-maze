8ffc39fd89a9b85144ca4bacce2f5691e87b284e
awohlford <34891927+awohlford@users.noreply.github.com>
2018-11-26 14:24:42 -0700

    Update node.py

--- node.py
+++ node.py
@@ -1,1 +1,10 @@
-
+class Node:
+  def __init__(self, num, color):
+    self.num = num
+    self.color = color
+    
+  def __eq__(self, node):
+    if self.num == node.num and self.color == node.color:
+      return True
+    else:
+      return False
