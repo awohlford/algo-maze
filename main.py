fe84067da23b211fc06049faaa7cffd076cf2001
awohlford <34891927+awohlford@users.noreply.github.com>
2018-11-26 01:31:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ first_line = input.readline().split()
 second_line = input.readline().split()
 for x in range(int(first_line[0]) - 1):
     graph.add_node((x + 1), color=second_line[x])
+    
+third_line = input.readline().split()
+rocket = third_line[0]
+lucky = third_line[1]
 
 for line in input.readlines():
     line = line.split()
