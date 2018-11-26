6c97eddd3619aeff91f92a0025273481cbcb64a0
awohlford <34891927+awohlford@users.noreply.github.com>
2018-11-26 14:20:31 -0700

    Update maze-regular.py

--- maze-regular.py
+++ maze-regular.py
@@ -1,1 +1,21 @@
+input = open("input_file.txt", "r")
+output = open("output_file.txt", "w")
 
+first_line = input.readline().split()
+n = first_line[0]
+m = first_line[1]
+
+second_line = input.readline().split()
+for x in range(n - 1):
+    
+    
+third_line = input.readline().split()
+rocket = third_line[0]
+lucky = third_line[1]
+
+for line in input.readlines():
+    line = line.split()
+    
+
+input.close()
+output.close()
