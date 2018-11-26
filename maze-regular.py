1e6d8f4e123ca2243872cde6539aac0710696762
awohlford <34891927+awohlford@users.noreply.github.com>
2018-11-26 14:28:44 -0700

    Update maze-regular.py

--- maze-regular.py
+++ maze-regular.py
@@ -1,13 +1,16 @@
 input = open("input_file.txt", "r")
 output = open("output_file.txt", "w")
 
+nodes = []
+edges = []
+
 first_line = input.readline().split()
 n = first_line[0]
 m = first_line[1]
 
 second_line = input.readline().split()
 for x in range(n - 1):
-    
+    nodes.append(Node(x + 1, second_line[x]))
     
 third_line = input.readline().split()
 rocket = third_line[0]
@@ -15,7 +18,11 @@ lucky = third_line[1]
 
 for line in input.readlines():
     line = line.split()
-    
+    edges.append(line[0], line[1], line[2])
 
 input.close()
 output.close()
+
+graph = Graph(nodes, edges)
+
+print(nodes, edges)
