c8ac2d3c9f93deb72ea238cb9f4bd1bcec213255
awohlford <34891927+awohlford@users.noreply.github.com>
2018-11-26 14:26:32 -0700

    Update edge.py

--- edge.py
+++ edge.py
@@ -1,1 +1,11 @@
+class Edge:
+  def __init__(self, start, end, color):
+    self.start = start
+    self.end = end
+    self.color = color
 
+  def __eq__(self, other):
+    if self.start == other.start and self.end == other.end and self.color == other.color:
+      return True
+    else:
+      return False
